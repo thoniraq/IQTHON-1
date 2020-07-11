bbda9e4deaf105540ac0f7e0ef8f10c5fc58cb45
klanrali <64720410+klanrali@users.noreply.github.com>
2020-07-12 01:55:25 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [Check Gu
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Currently Alive, my peru master!` **ψ(｀∇´)ψ**\n\n"
+    await alive.edit("`Currently Alive, my klanr master!` **ψ(｀∇´)ψ**\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
                      # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
-                     "`Bot created by:` [Dav](https://t.me//aaaDa), @telethon\n"
+                     "`Bot created by:` [Dav_klanr](https://t.me//aaaDa), @tele_thon\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
                      "https://github.com/klanrali/lite-telethon")
