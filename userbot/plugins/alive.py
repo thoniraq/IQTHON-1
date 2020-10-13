257e3da49595fdbf8adca30df806188914b5f37d
i.q <64720410+klanrali@users.noreply.github.com>
2020-10-14 02:32:20 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No Name set yet. Check Guide."
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("🇮🇶🕷️Hello Repo Telepython IQ \n\n"
-                     "🇮🇶🕷️Telepython version:1.0.0\nPython: 1.1.1\n"
-                     # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
-                     "🇮🇶🕷️created by: @iraqthon\n"
-                    "Iraq Thun files : @YZZZY\n"
-                     }\n")
+    await alive.edit("Hello Repo Telepython IQ 🇮🇶🕷️"
+                     "Telepython version🕷️🇮🇶: 1.0.0\nPython: 1.1.1\n"
+                     "Channel🕷️🇮🇶 : @iraqthon\n"
+                    "file🕷️🇮🇶 : @YZZZY\n"
+                     f"My peru owner: {DEFAULTUSER}\n\n"
+                     "https://github.com/klanrali/iraqthon")
