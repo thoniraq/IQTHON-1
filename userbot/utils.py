558982f884ff6281a507693ec61635bc6589bbd3
i.q <64720410+klanrali@users.noreply.github.com>
2020-10-14 14:27:42 +0300

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -18,7 +18,6 @@ from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
 from var import Var
 
-from . import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
 from .helpers.exceptions import CancelProcess
 
 ENV = bool(os.environ.get("ENV", False))
