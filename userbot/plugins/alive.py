305c51075ef93445caa2cbe58535853ff517457d
i.q <64720410+klanrali@users.noreply.github.com>
2020-10-15 11:53:30 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,13 +7,13 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [link rebo iraqthon.](https://github.com/klanrali/iraqthon)"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [rebo iraqthon🕷️🇮🇶](https://github.com/klanrali/iraqthon)"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Hello Repo Telepython IQ 🇮🇶🕷️`\n\n"
-                     "`iraqthon version🕷️🇮🇶: 1.0.0\nPython: 1.1.1\n`"
+    await alive.edit("⌔︙Hello Repo Telepython IQ 🇮🇶🕷️\n"
+                     "⌔︙iraqthon version🕷️🇮🇶: 1.0.0 Python: 1.1.1\n"
                      # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
-                      "`Bot created by:` 🕷️🇮🇶 [klanrali](tg://user?id=1226408155),🕷️🇮🇶 [telethon](https://t.me/TELE_THON)\n"
-                     f"`My peru owner`: {DEFAULTUSER}\n\n")
+                      "⌔︙Bot created by: [klanrali🕷️🇮🇶](tg://user?id=1226408155),[telethon🕷️🇮🇶](https://t.me/TELE_THON)\n"
+                     f"⌔︙My peru owner`: {DEFAULTUSER}\n\n")
