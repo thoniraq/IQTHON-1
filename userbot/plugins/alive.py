dd8d8afb10bf4e941fd1365bf77b75338b643209
i.q <64720410+klanrali@users.noreply.github.com>
2020-10-15 19:17:00 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,9 +12,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "[rebo 🕷️🇮🇶](https:/
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("⌔︙🕷🇮🇶 IRAQTHON/n"
-                     "⌔︙🕷🇮🇶 Version: 1.0.0/n"
+    await alive.edit("⌔︙🕷🇮🇶 IRAQTHON`\n\n"
+                     "⌔︙🕷🇮🇶 Version: 1.0.0\n`"
                      # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
-                      "⌔︙🕷🇮🇶 Created By: [KLANR](tg://user?id=1094825801),|| [CH IQ](https://t.me/TELE_THON)\n"
-                     f"⌔︙🕷🇮🇶 My owner : {DEFAULTUSER}\n")
+                      "⌔︙🕷🇮🇶 Created By: [KLANR](tg://user?id=1094825801) || [CH IQ](https://t.me/TELE_THON)\n`"
+                     f"⌔︙🕷🇮🇶 My owner : {DEFAULTUSER}\n`")
                      
