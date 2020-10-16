acb2bf316aded05f67f808e8183b92c95a1b3e3f
i.q <64720410+klanrali@users.noreply.github.com>
2020-10-16 11:50:11 +0300

    Update and rename time.py to time_IQ.py

--- userbot/plugins/time_IQ.py
+++ userbot/plugins/time_IQ.py
@@ -1,5 +1,6 @@
+#"""@iqthon iraq ©
 """ It does not do to dwell on dreams and forget to live
-Syntax: .getime"""
+Syntax: .الوقت"""
 
 import asyncio
 import os
@@ -11,11 +12,11 @@ from userbot.utils import admin_cmd
 FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
 
 
-@borg.on(admin_cmd("time ?(.*)"))  # pylint:disable=E0602
+@borg.on(admin_cmd("الوقت ?(.*)"))  
 async def _(event):
     if event.fwd_from:
         return
-    current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n⚡USERBOT TIMEZONE⚡ \n LOCATION: India \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
+    current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n⚡IQTHON TIME⚡ \n LOCATION: IRAQ \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
     start = datetime.now()
     input_str = event.pattern_match.group(1)
     reply_msg_id = event.message.id
@@ -24,20 +25,20 @@ async def _(event):
     elif event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         reply_msg_id = previous_message.id
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):  # pylint:disable=E0602
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
-    # pylint:disable=E0602
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY): 
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  
+    
     required_file_name = Config.TMP_DOWNLOAD_DIRECTORY + " " + str(datetime.now()) + ".webp"
     img = Image.new("RGBA", (350, 220), color=(0, 0, 0, 115))
     fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
     drawn_text = ImageDraw.Draw(img)
     drawn_text.text((10, 10), current_time, font=fnt, fill=(255, 255, 255))
     img.save(required_file_name)
-    await borg.send_file(  # pylint:disable=E0602
+    await borg.send_file(  
         event.chat_id,
         required_file_name,
-        caption="Userbot: Powered by @XtraTgBot",
-        # Courtesy: @ManueI15
+        caption="Userbot: Powered by @IQTHON",
+        
         reply_to=reply_msg_id
     )
     os.remove(required_file_name)
@@ -48,7 +49,7 @@ async def _(event):
     await event.delete()
 
 
-@borg.on(admin_cmd("gtime (.*)"))  # pylint:disable=E0602
+@borg.on(admin_cmd("gtime (.*)"))  
 async def _(event):
     if event.fwd_from:
         return
