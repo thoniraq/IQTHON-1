acfb1d3b2c4fada05451ab765a60a03f055db328
i.q <64720410+klanrali@users.noreply.github.com>
2020-10-16 12:51:38 +0300

    Update and rename alive.py to alive_IQ.py

--- userbot/plugins/alive_IQ.py
+++ userbot/plugins/alive_IQ.py
@@ -7,14 +7,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "[rebo 🕷️🇮🇶](https://github.com/klanrali/iraqthon)"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "[rebo 🕷️🇮🇶](https://github.com/klanrali/IQTHON)"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("⌔︙🕷🇮🇶 IRAQTHON`\n\n"
-                     "⌔︙🕷🇮🇶 Version: 1.0.0\n`"
+    await alive.edit("⌔︙🕷🇮🇶 IRAQTHON\n"
+                     "⌔︙🕷🇮🇶 Version: 1.0.0\n"
                      # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
-                      "⌔︙🕷🇮🇶 Created By: [KLANR](tg://user?id=1094825801) || [CH IQ](https://t.me/TELE_THON)\n`"
-                     f"⌔︙🕷🇮🇶 My owner : {DEFAULTUSER}\n`")
+                      "⌔︙🕷🇮🇶 Created By: [KLANR](tg://user?id=1094825801) || [CH IQ](https://t.me/IQTHON)\n"
+                     f"⌔︙🕷🇮🇶 My owner : {DEFAULTUSER}\n")
                      
