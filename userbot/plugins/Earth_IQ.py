f934e47dc2b104f677db556ec238fd79683ed6e7
i.q <64720410+klanrali@users.noreply.github.com>
2020-10-19 21:05:01 +0300

    Update and rename Earth.py to Earth_IQ.py

--- userbot/plugins/Earth_IQ.py
+++ userbot/plugins/Earth_IQ.py
@@ -1,12 +1,11 @@
-# (c) @UniBorg
-# Original written by @UniBorg edit by @I_m_Rock
+# This Source IQTHON AND DAV @KLANR
 
 from telethon import events
 import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.earth", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.الارض", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
